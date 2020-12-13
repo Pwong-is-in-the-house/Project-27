7dc7a2b05874735fc0445f1a407892ba91fbc9d4
Pwong-is-in-the-house <45134579+Pwong-is-in-the-house@users.noreply.github.com>
2020-12-13 13:52:40 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -50,4 +50,8 @@ function draw() {
   drawSprites();
  
 }
-
+function keyPressed(){
+if(keyCode=== UP_ARROW){
+	Matter.Body.applyForce(bobObject1.body,bobObject1.body.position,{x:-50,y:-45})
+}	
+}
