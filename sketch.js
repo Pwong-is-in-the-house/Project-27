8dfae618daccfc57d2c23f95cf3c0ccb37401713
Pwong-is-in-the-house <45134579+Pwong-is-in-the-house@users.noreply.github.com>
2020-12-13 13:47:32 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,25 +1,28 @@
 
+
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
+const Constraint= Matter.Constraint;
+const Render= Matter.Render;
 var bobObj,bobObj2, bobObj3, bobObj4, bobObj5;
 var roofObj;
 var rope1,rope2,rope3,rope4,rope5;
 
 function setup() {
-	createCanvas(800, 700);
+	createCanvas(1600, 900);
 
 
 	engine = Engine.create();
 	world = engine.world;
-
+	roofObj=new Roof(width/2, height/4, width/7,20)
+bobDiameter=40;
 	bobObj=new Bob(200,450,40);
 	bobObj2=new Bob(200,450,40);
 	bobObj3=new Bob(200,450,40);
 	bobObj4=new Bob(200,450,40);
 	bobObj5=new Bob(200,450,40);
-	roofObj=new Roof(width/2,200,width,20);
 	rope1=new Rope(bobObj.body,roofObj.body,-bobDiameter*2,0)
 	rope2=new Rope(bobObj2.body,roofObj.body,-bobDiameter*2,0)
 	rope3=new Rope(bobObj3.body,roofObj.body,-bobDiameter*2,0)
@@ -32,7 +35,7 @@ function setup() {
 
 function draw() {
   rectMode(CENTER);
-  background(0);
+  background(22);
   bobObj.display();
   bobObj2.display();
   bobObj3.display();
@@ -47,6 +50,4 @@ function draw() {
   drawSprites();
  
 }
-
-
 
